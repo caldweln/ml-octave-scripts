54fd8feadb67541630164cce378d551d0f28fd12
nick caldwell <nick.caldwell94@gmail.com>
2015-11-04 18:01:04 +0000

    minor changes to comments,formating etc

--- SupportVectorMachines/bruteForceParams.m
+++ SupportVectorMachines/bruteForceParams.m
@@ -4,38 +4,35 @@ function [C, sigma] = bruteForceParams(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-	% build steps {0.001, 0.003, 0.01, 0.03, ...}
+	% build steps {0.01, 0.03, 0.1, 0.3, ...}
 	steps = [power(10,-2:2); 3*power(10,-2:2)];
 	steps = steps(:);
 	step_count = length(steps);
 
 	err = 0;
 	best_err = realmax();
-	best_i = 1;
-	best_j = 1;
+	best_ind = ones(2,1);
 
 	% brute force, check performance for every C/sigma combination
 	for i=1:step_count
 		C = steps(i)
 		
 		for j=1:step_count
-		sigma = steps(j)
+			sigma = steps(j)
 
-		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-		predictions = svmPredict(model, Xval);
-		err = mean(double(predictions ~= yval))
+			model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+			predictions = svmPredict(model, Xval);
+			err = mean(double(predictions ~= yval))
 
 			if(err<best_err)
 				% new performance record
-				best_i = i;
-				best_j = j;
+				best_ind = [i;j];
 				best_err = err;
 			end
 		end
 	end
 
-	C = steps(best_i);
-	sigma = steps(best_j);
-
+	C = steps(best_ind(1,1));
+	sigma = steps(best_ind(2,1));
 
 end
